e5bad737844f1e7779b3d8756cba54b3520e74a8
Aytekinozis <aytekinozis93@gmail.com>
2023-12-19 18:11:17 +0300

    proje tamamlandi

---
+++ src/reducers/favoriteReducer.js
@@ -0,0 +1,44 @@
+import {
+  TOGGLE_FAVORITES,
+  REMOVE_FAVORITE,
+  ADD_FAVORITE,
+} from "../actions/favoritesActions";
+
+const initialState = {
+  favorites: [],
+  displayFavorites: true,
+};
+
+const favoriteReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_FAVORITE:
+      let newState = {};
+      if (state.favorites.find((item) => item.id === action.payload.id)) {
+        newState = { ...state };
+      } else {
+        newState = {
+          ...state,
+          favorites: [...state.favorites, action.payload],
+        };
+      }
+      return newState;
+
+    case REMOVE_FAVORITE:
+      return {
+        ...state,
+        favorites: state.favorites.filter((item) => action.payload !== item.id),
+      };
+
+    case TOGGLE_FAVORITES:
+      console.log("case");
+      return {
+        ...state,
+        displayFavorites: !state.displayFavorites,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default favoriteReducer;
